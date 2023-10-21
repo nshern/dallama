d50164790a98a4e880972fc84d6b0d35c8b28adf
Niklas Alexander Shern <nashern@protonmail.com>
2023-10-21 21:32:08 +0200

    feat: improved spell checker

--- spell_check.py
+++ spell_check.py
@@ -1,63 +1,65 @@
-# # Copyright (c) Microsoft Corporation. All rights reserved.
-# # Licensed under the MIT License.
-
-# # -*- coding: utf-8 -*-
-
-# import json
-# import os
-# from pprint import pprint
-# import requests
-
-# """
-# This sample makes a call to the Bing Web Search API with a query and returns relevant web search.
-# Documentation: https://docs.microsoft.com/en-us/bing/search-apis/bing-web-search/overview
-# """
-
-# # Add your Bing Search V7 subscription key and endpoint to your environment variables.
-# subscription_key = "35ef7ef31c7443aea671d7b390d7029b"
-# endpoint = "https://api.bing.microsoft.com/v7.0/spellcheck"
-
-
-# # Construct a request
-# params = {"text": "Facebok", "mode": ""}
-# headers = {"Ocp-Apim-Subscription-Key": subscription_key}
-
-# # Call the API
-# try:
-#     response = requests.get(endpoint, headers=headers, params=params)
-#     response.raise_for_status()
-
-#     # print("Headers:")
-#     # print(response.headers)
-
-#     print("JSON Response:")
-#     print(response.status_code)
-#     pprint(response.content)
-# except Exception as ex:
-#     raise ex
-
-
 import requests
+import os
 import json
+from langdetect import detect
+import time
 
-api_key = "35ef7ef31c7443aea671d7b390d7029b"
-example_text = "Hollo, wrld"
-example_text = "Hcad er den største by i verdn"
-with open("results/nous-hermes:7b.txt", "r") as f:
-    example_text = f.read()
-print(example_text)
-endpoint = "https://api.bing.microsoft.com/v7.0/SpellCheck"
 
-data = {"text": example_text}
+def detect_language(text):
+    return detect(text)
 
-params = {"mkt": "da-DK", "mode": "proof"}
 
-headers = {
-    "Content-Type": "application/x-www-form-urlencoded",
-    "Ocp-Apim-Subscription-Key": api_key,
-}
+def get_file_paths(directory):
+    file_paths = []
 
-response = requests.post(endpoint, headers=headers, params=params, data=data)
+    for root, directories, files in os.walk(directory):
+        for filename in files:
+            filepath = os.path.join(root, filename)
+            file_paths.append(filepath)
+    return file_paths
 
-json_response = response.json()
-print(json.dumps(json_response, indent=4))
+
+def evaluate_text(file):
+    api_key = "35ef7ef31c7443aea671d7b390d7029b"
+    endpoint = "https://api.bing.microsoft.com/v7.0/SpellCheck"
+
+    with open(file, "r") as f:
+        example_text = f.read().splitlines()
+
+    for i in example_text:
+        if i == "":
+            continue
+
+        else:
+            data = {"text": i}
+
+            params = {"mkt": "da-DK", "mode": "proof"}
+
+            headers = {
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Ocp-Apim-Subscription-Key": api_key,
+            }
+
+            while True:
+                response = requests.post(
+                    endpoint, headers=headers, params=params, data=data
+                )
+
+                if response.status_code != 429:
+                    break
+
+                print(
+                    "Rate limit exceeded. Waiting for 2 seconds before retrying.."
+                )
+                time.sleep(2)
+
+            json_response = response.json()
+            print(json.dumps(json_response, indent=4))
+            print(detect_language(i))
+
+
+if __name__ == "__main__":
+    files = get_file_paths("./results")
+    for file in files:
+        evaluate_text(file)
+    # print(detect_language("Hej mit navn er Kaj"))
