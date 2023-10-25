5ffc1b9149f256ce0504ba294f1e336320625a76
Niklas Alexander Shern <nashern@protonmail.com>
2023-10-25 12:35:20 +0200

    wip

--- main.py
+++ main.py
@@ -1,33 +1,30 @@
-import glob
 import os
 
 from model import Model
 
 
-def delete_all_files(directory):
-    files = glob.glob(directory + "/*")
-    for f in files:
-        os.remove(f)
-
-
-def clean_modefiles():
-    delete_all_files("./modelfiles")
+def clean_modelfiles(directory):
+    for f in os.listdir(directory):
+        os.remove(f"{directory}/{f}")
 
 
 base_models = [
     "mistral:7b",
-    "mistral:7b-text-fp16",
-    "llama2:7b",
-    "llama2:13b",
-    "vicuna:7b",
-    "vicuna:13b",
-    "nous-hermes:13b",
+    # "mistral:7b-text-fp16",
+    # "llama2:7b",
+    # "llama2:13b",
+    # "vicuna:7b",
+    # "vicuna:13b",
+    # "nous-hermes:13b",
 ]
 
 if __name__ == "__main__":
+    clean_modelfiles("./modelfiles")
+
     with open("prompt.md", "r") as f:
         prompt = f.read()
 
     for base_model in base_models:
         model = Model(base_model=base_model, system=prompt)
         model.create_modelfile("./modelfiles")
+        model.create_model_from_file()
