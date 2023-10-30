34fa72b3a1ff9953a885eb79bcffe20a1beef6d8
Niklas Alexander Shern <nashern@protonmail.com>
2023-10-30 12:50:20 +0100

    wip

--- test.py
+++ test.py
@@ -1,11 +1,26 @@
-import math
-
-import pandas as pd
 import requests
 
 r = requests.get("https:www.dr.dk")
 
-
-print(math.pi)
-
-df = pd.DataFrame
+f = [
+    "test",
+    "test",
+    "test",
+    "test",
+    "test",
+    "test",
+    "test",
+    "test",
+    "test",
+    "test",
+    "test",
+    "test",
+    "test",
+    "test",
+    "test",
+    "test",
+    "test",
+    "test",
+    "test",
+    "test",
+]
