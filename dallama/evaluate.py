15b1b34e7a6ab182490bd949cbc8138c7a0fa290
Niklas Alexander Shern <nashern@protonmail.com>
2023-12-06 09:41:09 +0100

    .

---
+++ dallama/evaluate.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+# def detect_language(df):
+#     df =
+
+
+def main():
+    df = pd.read_csv("../overview.py")
+    print(df.columns)
+
+
+if __name__ == "__main__":
+    main()
